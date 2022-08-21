c9465e312bab04e5311fcbf39c733a253eaad51f
Selena <selenadev096@gmail.com>
2022-08-21 17:31:20 -0500

    Creacion de carpetas

---
+++ EduTic.App/EduTic.App.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EduTic.App.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ EduTic.App/EduTic.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EduTic.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
